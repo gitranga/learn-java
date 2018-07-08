70175ac1bf3706402952e7d7dac91ebd4ca02a68
gitranga <40924812+gitranga@users.noreply.github.com>
2018-07-08 14:29:20 -0400

    learn data types
    learn data types

---
+++ datatypes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class datatypes {
+    public static void main (String args[]){
+        System.out.println(" Here are the list of data types and values  for learning");
+        char aletter = 'B';
+        System.out.println ("Character[ char] value " + aletter);
+        boolean isitdone = true;
+        System.out.println(" boolean [boolean] variable value  " + isitdone);
+        int distToPark = 100;
+        System.out.println("Integer [int] variable value " + distToPark);
+        byte color = 127;
+        System.out.println(" Byte [byte] variable value "+color);
+        short myage = 30;
+        System.out.println(" Short [ short ] variable value " + myage);
+        long dist = 1000000000;
+        System.out.println(" Long [long] variable value  "+dist);
+        float price = 100.45f;
+        System.out.println("float [float] variable value  " + price);
+        
+        double pivalue = Math.PI;
+        System.out.println("double [double] variable value  "+pivalue);
+        
+        /*
+        String type 
+        */
+        String myName = "myName";
+        System.out.println("String [String] is not primitive data type -" + myName);
+            
+    }
+}
